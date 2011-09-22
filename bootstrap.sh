4beaf697d1e07532345c073cb403115df28ef061
Kris Leech <kris.leech@interkonect.com>
2011-09-22 10:17:25 +0100

    ~/.vimrc will usually already exist

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cp -r ~/.vim ~/.vim.old 2>/dev/null
 rm -fr ~/.vim 2>/dev/null
+rm -f ~/.vimrc 2>/dev/null
 git clone https://github.com/krisleech/vimfiles ~/.vim && cd ~/.vim && rake install && ln -s ~/.vim/vimrc ~/.vimrc
