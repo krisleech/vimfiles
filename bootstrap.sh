4d761818ccfc64fc505b389d246bc6dfeffe4534
Errin Larsen <elarsen@insightcards.com>
2012-10-29 13:41:02 -0500

    Clean up bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 cp -r ~/.vim ~/.vim.old 2>/dev/null
 rm -fr ~/.vim 2>/dev/null
 rm -f ~/.vimrc 2>/dev/null
-git clone https://github.com/errinlarsen/vimfiles/tree/mine ~/.vim && cd ~/.vim && rake install && ln -s ~/.vim/vimrc ~/.vimrc
+git clone git@github.com:errinlarsen/vimfiles.git ~/.vim && cd ~/.vim && rake install && ln -s ~/.vim/vimrc ~/.vimrc
