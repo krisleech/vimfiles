da25dab3c7ad561e6ed9672e8f27de7d6ac6b98b
Errin Larsen <elarsen@insightcards.com>
2012-10-29 13:12:47 -0500

    Modify bootstrap.sh to refer to my own fork

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 cp -r ~/.vim ~/.vim.old 2>/dev/null
 rm -fr ~/.vim 2>/dev/null
 rm -f ~/.vimrc 2>/dev/null
-git clone https://github.com/krisleech/vimfiles ~/.vim && cd ~/.vim && rake install && ln -s ~/.vim/vimrc ~/.vimrc
+git clone https://github.com/errinlarsen/vimfiles/tree/mine ~/.vim && cd ~/.vim && rake install && ln -s ~/.vim/vimrc ~/.vimrc
